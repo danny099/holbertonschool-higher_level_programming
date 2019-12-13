c9b1055445353d201ac9754811340bdea6b06651
danny099 <dalmariv01@gmail.com>
2019-12-13 00:01:54 +0000

    exercise 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    count = len(sys.argv)
+    if count == 1:
+        print("0 arguments.")
+    if count == 2:
+        print("{} argument:".format(count - 1))
+    else:
+        print("{} arguments:".format(count - 1))
+    for i in range(1, count):
+        print("{}: {}".format(i, sys.argv[i]))
+
