eaef65ab35840c990f7b84216b93de131615871b
danny099 <dalmariv01@gmail.com>
2020-01-09 04:35:07 +0000

    exercise 101

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,9 +4,9 @@ from sys import stderr
 
 def safe_function(fct, *args):
     try:
-    res = fct(*args)
+        res = fct(*args)
     except BaseException as e:
         print("Exception: {}".format(e), file=stderr)
         res = None
         finally:
-        return res
+            turn res
