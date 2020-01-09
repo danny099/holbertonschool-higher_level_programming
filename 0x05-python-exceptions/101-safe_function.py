8536b4eefe21815c51a8600c13597a4167179fdf
danny099 <dalmariv01@gmail.com>
2020-01-09 04:31:55 +0000

    ecercises 102 ans 101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_function(fct, *args):
+    try:
+    res = fct(*args)
+    except BaseException as e:
+        print("Exception: {}".format(e), file=stderr)
+        res = None
+        finally:
+        return res
