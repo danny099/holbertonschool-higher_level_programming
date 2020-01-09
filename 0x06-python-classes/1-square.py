072d639388d6f196a1bbc3aadb3bc5788771ae5b
danny099 <dalmariv01@gmail.com>
2020-01-09 15:33:13 -0500

    comments

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """init
+        Description: init size
+        Args:
+             arg1 (size): size of square"""
+        self.__size = size
+
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+                """init
+        Description: init size
+        Args:
+             arg1 (size): size of square"""
+        if not(isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        if(size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    def area(self):
+        """Return area"""
+        return self.__size * self.__size
