0fc2718910af1eea0e5ff5034ab9eca01b9f0675
danny099 <dalmariv01@gmail.com>
2020-01-09 16:22:22 -0500

    exercise 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """init
+        Description: init size
+        Args:
+             arg1 (size): size of square"""
+        self.size = size
+    
+    def area(self):
+        """Return area"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """property"""
+        return self.__size
+    @size.setter
+    def size(self, value):
+        if not(isinstance(value, int)):
+            raise TypeError("size must be an integer")
+        if(value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
