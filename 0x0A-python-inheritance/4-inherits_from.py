6f39fc30d45fb6808e1bd71b360d078cf0f61ab6
danny099 <dalmariv01@gmail.com>
2020-01-20 16:24:00 -0500

    exercise 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+"""class"""
+
+
 def inherits_from(obj, a_class):
+    """function"""
+    if type(obj) != a_class and issubclass(type(obj), a_class):
+        return True
+    else:
+        return False
