05004d17d5c154fa81542282b9099261266f70fe
danny099 <dalmariv01@gmail.com>
2020-01-21 15:59:34 -0500

    exercise 2

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
+"""read lines"""
+
+
 def read_lines(filename="", nb_lines=0):
+    """read lines"""
+    with open(filename, 'r', encoding="utf-8") as f:
+        i = 0
+        for line in f:
+            i += 1
+        if nb_lines <= 0 or nb_lines >= i:
+            with open(filename, 'r', encoding="utf-8") as f:
+                for line in f:
+                    print(line, end="")
+        else:
+            with open(filename, 'r', encoding="utf-8") as f:
+                for lines in range(nb_lines):
+                    print(f.readline(), end="")
