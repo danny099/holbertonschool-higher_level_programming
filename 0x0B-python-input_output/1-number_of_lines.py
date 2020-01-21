ed334f56ba410216bd5bc483a6ebbbf23fe5db01
danny099 <dalmariv01@gmail.com>
2020-01-21 15:28:26 -0500

    exercise 1

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+"""number of lines"""
+
+
 def number_of_lines(filename=""):
+    """number of lines"""
+    i = 0
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            i += 1
+        return i
