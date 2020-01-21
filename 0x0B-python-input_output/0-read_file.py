24aeb13d25028dcde50088469325c203a53541f0
danny099 <dalmariv01@gmail.com>
2020-01-21 17:22:30 -0500

    exercise 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,5 @@
 
 def read_file(filename=""):
     """read file"""
-    with open(filename, 'r' encoding="utf-8") as f:
-        for line in f:
-            print(line, end="")
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read(), end="")
