a7a24db4f5cacb459fc4ec266b75ac2269fc896f
danny099 <dalmariv01@gmail.com>
2020-01-21 17:10:07 -0500

    exercise 8

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""json"""
+import json
+
+
 def load_from_json_file(filename):
+    """json"""
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)
