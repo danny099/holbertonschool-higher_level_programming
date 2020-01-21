7bc1995258e037ac2d976a5d5e2f1475e50e7f40
danny099 <dalmariv01@gmail.com>
2020-01-21 16:14:36 -0500

    exercise 3

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+"""write file"""
+
+
 def write_file(filename="", text=""):
+    """write a file"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
