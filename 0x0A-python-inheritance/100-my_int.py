4a4dbd4b873158171ef24ed26d96c89b87960812
danny099 <dalmariv01@gmail.com>
2020-01-21 05:09:57 +0000

    exercise 100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""class."""
+
+
+class MyInt(int):
+    """class"""
+    def __ne__(self, thing):
+        return super().__eq__(thing)
+
+    def __eq__(self, thing):
+        return super().__ne__(thing)
