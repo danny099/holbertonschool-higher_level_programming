5260af0932b48696f442c4b5a68686a6664191e9
danny099 <dalmariv01@gmail.com>
2020-01-21 18:49:03 -0500

    exercise 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
-def class_to_json(obj):
+"""json"""
+import sys
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+
+try:
+    obj = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    save_to_json_file([], "add_item.json")
+    obj = load_from_json_file("add_item.json")
+
+for i in range(1, len(sys.argv)):
+    obj.append(str(sys.argv[i]))
+save_to_json_file(obj, "add_item.json")
