8feef5be3d00a6b8f95b2cb5ab8d1e49453651d1
danny099 <dalmariv01@gmail.com>
2020-01-21 16:20:19 -0500

    exercise 4

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+"""apeend write"""
+
+
 def append_write(filename="", text=""):
+    """append write"""
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
