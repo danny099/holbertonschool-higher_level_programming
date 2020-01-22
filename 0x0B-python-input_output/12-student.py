4b01a1428a436858126138f7505c11789dbc626a
danny099 <dalmariv01@gmail.com>
2020-01-22 04:18:39 +0000

    exercise 12

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""class"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """json"""
+        if attrs is None:
+            return self.__dict__
+        dic = {}
+        for i in attrs:
+            try:
+                dic[i] = self.__dict__[i]
+            except:
+                pass
+        return dic
