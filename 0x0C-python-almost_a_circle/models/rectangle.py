ab8de32ed8ea2fa044726991ed481176a1ea00d6
danny099 <dalmariv01@gmail.com>
2020-01-27 13:19:40 -0500

    comments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-is a reactangle class
+module for Reactangle class
 """
 
 
@@ -9,10 +9,10 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """is a Rectangle"""
+    """one base"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        "init a rectangle"
+        """init a base"""
         self.__width = width
         self.__height = height
         self.__x = x
