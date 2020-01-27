88351f8d549eb82903251aa780311167facd863c
danny099 <dalmariv01@gmail.com>
2020-01-27 13:36:03 -0500

    comments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -29,7 +29,7 @@ class Rectangle(Base):
         """setter function"""
         if type(value) != int:
             raise TypeError("width must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
@@ -43,7 +43,7 @@ class Rectangle(Base):
         """setter function"""
         if type(value) != int:
             raise TypeError("height must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
@@ -57,7 +57,7 @@ class Rectangle(Base):
         """setter function"""
         if type(value) != int:
             raise TypeError("x must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
@@ -71,7 +71,7 @@ class Rectangle(Base):
         """setter function"""
         if type(value) != int:
             raise TypeError("y must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
 
