cfc06a3428c93feeb28860c5e09bc329a83fb598
danny099 <dalmariv01@gmail.com>
2020-01-27 06:16:09 +0000

    exercise 14

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -49,3 +49,11 @@ class Square(Rectangle):
                 self.x = value
             if key == "y":
                 self.y = value
+        
+    def to_dictionary(self):
+        dictionary = {}
+        dictionary["id"] = self.id
+        dictionary["size"] = self.width
+        dictionary["x"] = self.x
+        dictionary["y"] = self.y
+        return dictionary
