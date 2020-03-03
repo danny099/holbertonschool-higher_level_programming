a0bf0f5e1a79b54ed7ef836cc3cca29868fb4a97
danny099 <dalmariv01@gmail.com>
2020-03-03 10:18:22 -0500

    all

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- generate permisions to users
+-- generate permisions to users
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
