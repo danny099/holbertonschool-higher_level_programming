487d5f2acc0f69c8d52dc09f7711c4c8a264fa24
danny099 <dalmariv01@gmail.com>
2020-03-03 16:32:37 -0500

    subquery

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,5 @@
 -- create a subquery for a list cities by state
 -- create a subquery for a list cities by state
-SELECT cities.id, cities.name FROM cities WHERE state_id = (SELECT id FROM states where name = "California") ORDER BY cities.id ASC;
+SELECT cities.id, cities.name 
+FROM cities 
+WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY cities.id ASC;
