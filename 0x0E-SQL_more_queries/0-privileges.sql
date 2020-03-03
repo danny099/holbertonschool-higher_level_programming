1e1db74f57f33018eadd5b1112572e5225aa9e34
danny099 <dalmariv01@gmail.com>
2020-03-03 13:07:06 -0500

    fix error

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- generate permisions to users
 -- generate permisions to users
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
