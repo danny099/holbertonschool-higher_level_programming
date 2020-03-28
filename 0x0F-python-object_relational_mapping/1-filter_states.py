19d6cb5c567d174b7cd46a298cb01167f9eb1a6a
danny099 <dalmariv01@gmail.com>
2020-03-28 03:02:20 -0500

    simple querys

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""gett all states of the table hbtn_0e_0_usa"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    conectDB = MySQLdb.connect(host='localhost', user=user, passwd=passwd, db=db, port=3306)
+    cur = conectDB.cursor()
+    cur.execute("SELECT * FROM states where name like 'N%' ORDER BY states.id;")
+    states = cur.fetchall()
+    for i in states:
+        print(i)
+    cur.close()
+    conectDB.close()
