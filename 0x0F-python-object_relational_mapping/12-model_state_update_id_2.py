e473c4520c6a9420f0e8481c02b5719ec8d6d048
danny099 <dalmariv01@gmail.com>
2020-03-29 23:40:21 -0500

    update object

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""insert object"""
+from sqlalchemy import func
+from model_state import State
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(user, passwd, db),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).filter_by(id=2).all()
+    for i in query:
+        i.name = 'New Mexico'
+    session.commit()
+    session.close()
