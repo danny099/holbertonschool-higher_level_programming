05547428414222ce502c5c5eb922076a2c58cf08
danny099 <dalmariv01@gmail.com>
2020-03-29 23:12:45 -0500

    filter by a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""objects of a table"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(user, passwd, db),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    sql = session.query(State).order_by(State.id).filter(State.name.like('%a%'))
+    for i in sql:
+        print("{}: {}".format(i.id, i.name))
+    session.close()
