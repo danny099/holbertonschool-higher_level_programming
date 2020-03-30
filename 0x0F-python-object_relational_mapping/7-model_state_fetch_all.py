6c26b04cbf3851cb65fd530290e4ec39928d91d0
danny099 <dalmariv01@gmail.com>
2020-03-29 22:33:14 -0500

    objects in a table

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""objects of a table"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(user, passwd, db),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for i in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(i.id, i.name))
+    session.close()
