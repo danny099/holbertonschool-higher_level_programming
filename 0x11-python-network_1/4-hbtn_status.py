2813082f5e745f7db1eca5dd10db20ec869d0e63
danny099 <dalmariv01@gmail.com>
2020-04-15 21:45:53 -0500

    status 1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""take url and print the info"""
+import requests
+if __name__ == "__main__":
+    html = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(html.text)))
+    print('\t- content: {}'.format(html.text))
