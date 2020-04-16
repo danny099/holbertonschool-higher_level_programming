e9f65d644dc23ff1695ac753664eac93e4d992e1
danny099 <dalmariv01@gmail.com>
2020-04-16 11:36:06 -0500

    json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""json api"""
+import requests
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) >= 2:
+        data = {'q': '{}'.format(argv[1])}
+    else:
+        data = {'q': ''}
+    r = requests.post('http://0.0.0.0:5000/search_user', data=data)
+    user = r.json()
+    try:
+        if user:
+            print("[{}] {}".format(user.get('id'), user.get('name')))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
