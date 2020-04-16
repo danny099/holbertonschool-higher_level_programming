bb25db506cf8bfc449762bebef964a7f81b35e4d
danny099 <dalmariv01@gmail.com>
2020-04-16 15:38:22 -0500

    commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,10 +3,8 @@
 import requests
 from sys import argv
 if __name__ == "__main__":
-    r = requests.get(
-        """
-        https://api.github.com/repos/{}/{}/commits?per_page=10
-        """.format(argv[2], argv[1]))
+    r = requests.get('https://api.github.com/repos/{}/{}/commits?per_page=10'
+                     .format(argv[2], argv[1]))
     for i in r.json():
         print("{}: {}".format(i.get('sha'), i.get(
             'commit').get('author').get('name')))
