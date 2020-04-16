4a14c84307a5ea83229b4a8c5fc22a5c0fdc15cf
danny099 <dalmariv01@gmail.com>
2020-04-16 10:36:15 -0500

    post email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""post email"""
+import requests
+from sys import argv
+if __name__ == "__main__":
+    email = {'email': '{}'.format(argv[2])}
+    r = requests.post(argv[1], data=email)
+    print(r.text)
