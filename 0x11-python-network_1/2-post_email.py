3937ac798a6e530a70afbaef1936d8acbba6ff78
danny099 <dalmariv01@gmail.com>
2020-04-15 21:01:12 -0500

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""post email"""
+import urllib.parse
+import urllib.request
+from sys import argv
+if __name__ == "__main__":
+    url = argv[1]
+    values = {'email': '{}'.format(argv[2])}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')  # data should be bytes
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        email = response.read()
+    print(email)
