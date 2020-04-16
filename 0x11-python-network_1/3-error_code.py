3f4bf6e68c778620574fa2a4ff49b041aabfdc2c
danny099 <dalmariv01@gmail.com>
2020-04-15 21:32:58 -0500

    error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""error code"""
+import urllib.request
+from sys import argv
+if __name__ == "__main__":
+    url = argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            body = response.read()
+        print(body.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(e.code)
