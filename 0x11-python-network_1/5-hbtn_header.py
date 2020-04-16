b00a6da768804fde65eaa1be79b3fad2bcaa2785
danny099 <dalmariv01@gmail.com>
2020-04-16 10:24:51 -0500

    headers

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""heraders"""
+import requests
+from sys import argv
+if __name__ == "__main__":
+    url = argv[1]
+    header = requests.get(url)
+    print(header.headers['X-Request-Id'])
