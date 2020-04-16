0ce3512ab1c493985e888f5411d71527cf458d1d
danny099 <dalmariv01@gmail.com>
2020-04-16 14:41:44 -0500

    my github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""profile id github"""
+import requests
+from sys import argv
+if __name__ == "__main__":
+    r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    print(r.json().get('id'))
