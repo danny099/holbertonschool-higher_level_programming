ae2c5ef997972f1d4dde60bd404ddbc1ddc175ca
danny099 <dalmariv01@gmail.com>
2020-04-16 10:54:56 -0500

    error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""heraders"""
+import requests
+from sys import argv
+if __name__ == "__main__":
+    url = argv[1]
+    r = requests.get(url)
+    if int(r.status_code) < 400:
+        print(r.text)
+    else:
+        print("Error code: {}".format(r.status_code))
