d3a1926b63e66e877684a46a4fd1c4da4c0f52f0
danny099 <dalmariv01@gmail.com>
2020-04-16 11:48:24 -0500

    json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,9 +4,9 @@ import requests
 from sys import argv
 if __name__ == "__main__":
     if len(argv) == 1:
-        letter = argv[1]
-    else:
         letter = ''
+    else:
+        letter = argv[1]
     r = requests.post('http://0.0.0.0:5000/search_user',
                       data={'q': '{}'.format(letter)})
     user = r.json()
