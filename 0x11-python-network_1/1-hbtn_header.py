65357623b69cf35963b83a41e001693b85e0846d
danny099 <dalmariv01@gmail.com>
2020-04-15 20:36:44 -0500

    header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""value of header"""
+import urllib.request
+from sys import argv
+if __name__ == "__main__":
+    url = argv[1]
+    with urllib.request.urlopen(url) as response:
+        header = response.getheader("X-Request-Id")
+    print(header)
