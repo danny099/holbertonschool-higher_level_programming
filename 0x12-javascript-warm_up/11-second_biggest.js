6e8648d782ac80b072b174d6ad14e1df23096c8c
danny099 <dalmariv01@gmail.com>
2020-05-05 06:55:41 -0500

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const len = process.argv.length;
+if (len <= 2) {
+  console.log(0);
+} else if (len === 3) {
+  console.log(0);
+} else {
+  secondBiggest();
+}
+
+function secondBiggest () {
+  const second = process.argv.sort(function (a, b) { return b - a; });
+  console.log(second[3]);
+}
