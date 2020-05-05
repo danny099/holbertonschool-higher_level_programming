47299c151e2f7d7afe5dcb5f3dca1d44f847d77f
danny099 <dalmariv01@gmail.com>
2020-05-05 06:36:06 -0500

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const number = parseInt(process.argv[2]);
+function factorial (x) {
+  if (process.argv.length <= 2 || x === 0) {
+    return 1;
+  }
+  return (x * factorial(x - 1));
+}
+console.log(factorial(number));
