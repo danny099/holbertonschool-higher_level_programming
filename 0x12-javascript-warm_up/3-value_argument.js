c48a3c84cb7890beb3129f7b143837abfe39fa29
danny099 <dalmariv01@gmail.com>
2020-05-05 02:41:00 -0500

    task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+let count = 0;
+process.argv.forEach((value, i) => {
+  count = i;
+});
+
+if (count <= 1) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
