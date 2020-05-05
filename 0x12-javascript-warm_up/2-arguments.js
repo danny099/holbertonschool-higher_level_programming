0996fbdc3f07818ee6cbba18f72a1b6da4c2e615
danny099 <dalmariv01@gmail.com>
2020-05-05 02:24:40 -0500

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+let count = 0;
+process.argv.forEach((value, i) => {
+  count = i;
+});
+
+if (count <= 1) {
+  console.log('No argument');
+} else if (count === 2) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
