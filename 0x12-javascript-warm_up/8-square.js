1f77a81d8581f2bc5c6d05fccf81a402b8bafeb4
danny099 <dalmariv01@gmail.com>
2020-05-05 03:22:51 -0500

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const number = parseInt(process.argv[2]);
+let square = '';
+if (Number.isInteger(number)) {
+  for (let i = 0; i < number; i++) {
+    for (let j = 0; j < number; j++) {
+      square += 'X';
+    }
+    if (i < number - 1) {
+      square += '\n';
+    }
+  }
+  console.log(square);
+} else {
+  console.log('Missing size');
+}
