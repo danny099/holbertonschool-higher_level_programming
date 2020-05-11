58e389fd374c53e7d76164a93089ff497fa54377
danny099 <dalmariv01@gmail.com>
2020-05-11 02:35:29 -0500

    task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,28 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    let rectangle = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        rectangle += 'X';
+      }
+      if (i < this.height - 1) {
+        rectangle += '\n';
+      }
+    }
+    console.log(rectangle);
+  }
+}
+
+const r1 = new Rectangle(2, 3);
+r1.print();
+
+const r2 = new Rectangle(10, 5);
+r2.print();
