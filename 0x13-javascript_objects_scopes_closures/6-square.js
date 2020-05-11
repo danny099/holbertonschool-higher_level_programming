4ce78deff5e2694fbcff96dbe1ff6ab15e061141
danny099 <dalmariv01@gmail.com>
2020-05-11 17:19:47 -0500

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+const square = require('./5-square');
+
+class Square extends square {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint(c){
+    let rectangle = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        if (c) {
+          rectangle += c;
+        } else {
+          rectangle += 'X';
+        }   
+      }
+      if (i < this.height - 1) {
+        rectangle += '\n';
+      }
+    }
+    console.log(rectangle);
+  }
+}
+
+module.exports = Square;
