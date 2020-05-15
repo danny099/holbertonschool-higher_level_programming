99a8a445fa5123a9cd0cdb959722d3a2ef18d116
danny099 <dalmariv01@gmail.com>
2020-05-15 00:44:59 -0500

    task8

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+$.getJSON('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+  data.results.forEach(function (peli) {
+    $('UL#list_movies').append('<li>' + peli.title + '</li>');
+  });
+});
